454bd5dbf505cc9186c367375cd60a7a73a9ad52
Alfian Hermawan <alfianhermawan444@gmail.com>
2023-11-27 12:37:59 +0700

    test3

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.ws('/:cameraIP', (ws, req) => {
     })(ws);
 });
 app.listen(2000, () => {
-    console.log('Alamat IPv4 pada mesin:');
+    console.log('IPv4 address on the machine:');
     for (const key in interfaces) {
         for (const iface of interfaces[key]) {
             if (iface.family === 'IPv4' && !iface.internal) {
