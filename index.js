6ed6a47a5fc548ee4937fb54f83ef9c7d2912f28
Alfian Hermawan <alfianhermawan444@gmail.com>
2023-11-27 15:51:03 +0700

    update port

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express'),
     app = express(),
     { proxy, scriptUrl } = require('rtsp-relay')(app),
     os = require('os'),
-    interfaces = os.networkInterfaces();
-addresses = [],
-    PORT = 2000;
+    interfaces = os.networkInterfaces(),
+    args = process.argv.slice(2),
+    getPort = parseInt(args[0]);
+var PORT = 2000;
 app.ws('/:cameraIP', (ws, req) => {
     console.log(`request from ${req.headers.origin}`);
     var cameraIP = decodeURIComponent(req.params.cameraIP);
@@ -15,12 +16,14 @@ app.ws('/:cameraIP', (ws, req) => {
         verbose: true,
     })(ws);
 });
-app.listen(2000, () => {
+if (getPort != null&&Number.isInteger(getPort)) { 
+    PORT = getPort;
+}
+app.listen(PORT, () => {
     console.log('IPv4 address on the machine:');
     for (const key in interfaces) {
         for (const iface of interfaces[key]) {
             if (iface.family === 'IPv4' && !iface.internal) {
-                addresses.push(iface.address);
                 console.log(`${iface.address}:${PORT}`);
             }
         }
